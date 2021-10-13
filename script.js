e494db81d9e040a2d9785245c9a4fb94c0517e9c
nehemiah-bit <>
2021-10-13 15:32:21 +0800

    update files

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){
-    // Display the date of today
+    // Display the date of the day
     document.querySelector("date").innerHTML += Date();
 });
